51fcdfc2e1ec74423586dab388275f8588f7dd22
Tom Casavant <tfcasavant@gmail.com>
2019-05-15 17:57:24 -0400

    Added ability to upload images

--- plugins/ExamplePlugin/ExamplePlugin.py
+++ plugins/ExamplePlugin/ExamplePlugin.py
@@ -4,3 +4,6 @@ class ExamplePlugin(IPlugin):
 	def process(self, msg, client):
 		response = f"I have received your message, {msg['name']}"
 		client.send_message(response)
+		data = open('plugins/ExamplePlugin/test.jpg', 'rb').read()
+		image_url = client.upload_image(data, "image/jpeg")
+		client.send_message_with_image(image_url, "Test image")
